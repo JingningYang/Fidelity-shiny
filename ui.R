78b0af6a9cbcf0cd06d4888e351233652734cb5f
JingningYang <54416899+JingningYang@users.noreply.github.com>
2019-11-09 11:15:29 -0500

    Add files via upload

--- ui.R
+++ ui.R
@@ -10,49 +10,47 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(tagList(fluidPage(#tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.min.css")), #First way to style
-  #theme = "bootstrap.min.css",   #Second way to style
-  #tags$head(tags$style(HTML("body{ background: green; }"))), #Third way to style ? 
-  
-  
-  #Should we only use green background or still try to figure out how to use boostrap.min.css make
-  #the position become horizontal rather than vertial? 
-  
-  navbarPage("Fidelity", #position = "fixed-top", inverse = T, 
-             tabPanel("Home",
-                      tags$br(),
-                      tags$br(),
-                      tags$h2("Two-semester Fidelity Project for Mutual Funds: Sector, Bond, and International"),
-                      tags$br(),
-                      tags$h3("Our Group:")       
-             ),
-             
-             
-             tabPanel("Sector Fund", 
-                      mainPanel(
-                        tabsetPanel(
-                          tabPanel("Tab 1",
-                                   h4("Table"),
-                                   tableOutput("table")))
-                      )),
-             
-             
-             tabPanel("International Fund", 
-                      tags$br(),
-                      tags$h2("Explanation"),
-                      sidebarPanel(
-                        selectInput("funds", "Selected International Funds:", c("FWWFX","FHKCX","FIVLX","FIVFX")),
-                        sliderInput("nav", "NAV(net asset value per share)",0,10000,0),
-                        selectInput("index", "Indices", c("MAKE A DATA FRAME?")),
+shinyUI(tagList(fluidPage(#tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.min.css")), #First way to style CSS file
+                          theme = "bootstrap.css",   #Second way(easiest way) to style CSS file
+                          #tags$head(tags$style(HTML("body{ background: green; }"))), #Change background color manually, but not beautiful. 
+                          
+#Should we only use green background or still try to figure out how to use boostrap.min.css and make the position become horizontal rather than vertial? 
+     
+                  navbarPage("Fidelity", #position = "fixed-top", inverse = T, 
+                        tabPanel("Home",
+                                 tags$br(),
+                                 tags$br(),
+                              tags$h2("Two-semester Fidelity Project for Mutual Funds: Sector, Bond, and International"),
+                              tags$br(),
+                              tags$h3("Our Group:")       
+                             ),
                         
-                        tags$h5(),
-                        actionButton("action", "Search"),
                         
-                        tags$h5("actionButton:"),
-                        actionButton("action2", "Action button", class = "btn-primary")
-                      )),
-             
-             
-             tabPanel("Bond Fund")
-  )
+                         tabPanel("Sector Fund", 
+                                      mainPanel(
+                                          tabsetPanel(
+                                              tabPanel("Tab 1",
+                                                       h4("Table"),
+                                                       tableOutput("table")))
+                                      )),
+                        
+                        
+                          tabPanel("International Fund", 
+                                   tags$br(),
+                                   tags$h2("Explanation"),
+                                   sidebarPanel(
+                                     selectInput("funds", "Selected International Funds:", c("FWWFX","FHKCX","FIVLX","FIVFX")),
+                                     sliderInput("nav", "NAV(net asset value per share)",0,10000,0),
+                                     selectInput("index", "Indices", c("MAKE A DATA FRAME?")),
+                               
+                               tags$h5(),
+                               actionButton("action", "Search"),
+                               
+                               tags$h5("actionButton:"),
+                               actionButton("action2", "Action button", class = "btn-primary")
+                             )),
+                        
+                        
+                          tabPanel("Bond Fund")
+                  )
 )))
