2fea992e43533a0eae7b05e5beb49f31ecf7adab
Yuanyuan Lin <yuanyuanlin@dhcp-wifi-8021x-155-41-108-168.bu.edu>
2019-11-12 16:55:37 -0500

    commit

--- ui.R
+++ ui.R
@@ -1,5 +1,66 @@
 library(shiny)
 
+
+prices_choices <-
+  c("FBNDX" = "FBNDX",
+    "Qundl" = "qundl",
+    "Alpha Vantage" = "alphavantage")
+prices_chart_types <- c(
+  "Bar Chart" = "bars",
+  "Line Chart" = "line",
+  "Match Sticks" = "matchsticks",
+  "Candlestick Chart" = "candlesticks"
+)
+prices_chart_themes <- c("White" = "white",
+                         "Black" = "black")
+indicators <- c(
+  "Moving Average" = "addSMA",
+  "Welles Wilder's Directional Movement Indicator" = "addADX",
+  "Average True Range" = "addATR",
+  "Bollinger Bands" = "addBBands",
+  "Bollinger Band Width" = "addBBands2",
+  "Bollinger %b" = "addBBands3",
+  "Commodity Channel Index" = "addCCI",
+  "Chaiken Money Flow" = "addCMF",
+  "Chande Momentum Oscillator" = "addCMO",
+  "Double Exponential Moving Average" = "addDEMA",
+  "Detrended Price Oscillator" = "addDPO",
+  "Exponential Moving Average" = "addEMA",
+  "Price Envelope" = "addEnvelope",
+  "Exponential Volume Weigthed Moving Average" = "addEVWMA",
+  "Options and Futures Expiration" = "addExpiry",
+  "Moving Average Convergence Divergence" = "addMACD",
+  "Momentum" = "addMomentum",
+  "Rate of Change" = "addROC",
+  "Relative Strength Indicator" = "addRSI",
+  "Parabolic Stop and Reverse" = "addSAR",
+  "Stocastic Momentum Index" = "addSMI",
+  "Triple Smoothed Exponential Oscillator" = "addTRIX",
+  "Weighted Moving Average" = "addWMA",
+  "Williams %R" = "addWPR",
+  "ZLEMA" = "addZLEMA"
+)
+return_features <- c(
+  "Open" = "open",
+  "Close" = "close",
+  "High" = "high",
+  "Low" = "low",
+  "Adjusted" = "adjusted",
+  "Volume" = "volume"
+)
+return_options <- c("Log" = "log",
+                    "Arithmetic" = "arithmetic")
+return_period_options <- c(
+  "Yearly" = "yearly",
+  "Quarterly" = "quarterly",
+  "Monthly" = "monthly",
+  "Weekly" = "weekly",
+  "Daily" = "daily"
+)
+signals <- c("Comparison" = "sigComparison",
+             "Crossover" = "sigCrossover")
+relations <- c("Less than" = "lt", "Greater Than" = "gt")
+
 title <- tags$a(href="https://www.fidelity.com", 
                 tags$img(src="Boston_University_Terriers_logo.jpg", height="55", width="80"),
                 tags$img(src="Fid_Logo_Rev_green.jpg", height="55", width="130"))
@@ -19,7 +80,7 @@ shinyUI(tagList(fluidPage(theme = "bootstrap.css",
                                   sidebarPanel(
                                     selectInput("sector","Sector Funds:",c("FSAIX","FRESX","FREMEX","FSDCX","FSPCX")),
                                     dateRangeInput("date", strong("Date range"), start = "2014-01-01", end = "2018-12-31",
-                                                   min = "2014-01-01", max = "2018-12-31"),
+                                                   min = "2014-01-01", max = "2018-12-31")
                                   )),
 
                           tabPanel("International Fund", 
@@ -49,8 +110,141 @@ shinyUI(tagList(fluidPage(theme = "bootstrap.css",
                              
             ),
                         
-                        
-                        
-                          tabPanel("Bond Fund")
+                          tabPanel(title="Bond Fund",value = "Prices",
+            tabsetPanel(
+              type = "tabs",
+              tabPanel(
+                title = "Buy & Sell Simulator",
+                value = "indi_sig",
+                wellPanel(
+                  fluidRow(
+                    column(
+                      width = 2,
+                      radioButtons(
+                        "pricesSource",
+                        label = h3("Bond Sticker"),
+                        choices = prices_choices
+                      )
+                    ),
+                    column(
+                      width = 2,
+                      radioButtons(
+                        "pricesChartType",
+                        label = h3("Chart Type"),
+                        choices = prices_chart_types,
+                        selected = "candlesticks"
+                      )
+                    ),
+                    column(
+                      width = 2,
+                      radioButtons(
+                        "pricesChartTheme",
+                        label = h3("Chart Theme"),
+                        choices = prices_chart_themes
+                      )
+                    ),
+                    column(
+                      width = 2,
+                      h3("Miscellaneous"),
+                      checkboxInput("priceChartGrid",
+                                    "Show Grid",
+                                    value = TRUE),
+                      checkboxInput("priceLogScale",
+                                    "Log Scale",
+                                    value = FALSE)
+                    ),
+                    column(
+                      width = 4,
+                      h3("Simulation Setup"),
+                      selectInput("indicators", "Indicators",
+                                  indicators , multiple = TRUE),
+                      dateRangeInput(
+                        inputId = "daterange",
+                        label = "Date range",
+                        start = "2014-01-01",
+                        end = "2018-12-31"
+                      ),
+                      actionButton("predict_btn",
+                                   "Simulate")
+                    )
+                  )),
+                fluidRow(column(width = 12,
+                                plotOutput("prices_plot"))),
+                conditionalPanel("input.predict_btn",
+                                 hr(),
+                                 fluidRow(column(width = 12,
+                                                 verbatimTextOutput("simulator_summary"))),
+                                 hr(),
+                                 fluidRow(column(width = 12,
+                                                 h4("Trading Evaluation"),
+                                                 verbatimTextOutput("trading_evaluation"))),
+                                 hr(),
+                                 
+                                 fluidRow(column(width = 12,
+                                                 h4("Downside Risk"),
+                                                 verbatimTextOutput("downside_risk"))),
+                                 hr(),
+                                 fluidRow(column(width = 12,
+                                                 plotOutput("buy_sell_plot"))),
+                                 hr(),
+                                 fluidRow(column(width = 12,
+                                                 plotOutput("CumReturns"))),
+                                 hr(),
+                                 fluidRow(column(width = 12,
+                                                 plotOutput("yearlyReturn"))),
+                                 hr(),
+                                 fluidRow(column(width = 12,
+                                                 plotOutput("rets"))))
+              ),
+              tabPanel(
+                title = "Periodic Returns",
+                value = "perdioic_returns",
+                sidebarLayout(
+                  sidebarPanel(
+                    dateRangeInput(
+                      inputId = "daterange_return",
+                      label = "Date range",
+                      start = "2014-01-01",
+                      end = "2018-12-31"
+                    ),
+                    selectInput("return_features", "Apply return to",
+                                return_features),
+                    selectInput("return_function", "Transformation Function",
+                                return_options),
+                    selectInput(
+                      "return_period",
+                      "Transformation Priod",
+                      return_period_options,
+                      selected = "yearly"
+                    )
+                  ),
+                  mainPanel(plotOutput("return_plot"))
+                )
+              ),
+              tabPanel(
+                title = "Simulation",
+                value = "simulation",
+                sidebarLayout(
+                  sidebarPanel(
+                    numericInput("days_num", "Number of Bond Price Simulations", value = 252),
+                    numericInput("sim_num", "Number of Monte Carlo Simulations", value = 250)
+                  ),
+                  mainPanel(
+                    plotOutput("daily_return_plot"),
+                    plotOutput("simulation_plot")
                   )
-)))
+                )
+              ),
+              tabPanel(title = "Dataset download", icon =  icon("download"), value = 8,
+                       p(class = "lead","Download complete dataset"),
+                       br(),
+                       downloadButton(outputId = "downloadExpressionData", label = "Download expression data", class= "btn-primary"),
+                       p(""),
+                       downloadButton(outputId = "downloadpData", label = "Download pheno data", class= "btn-primary")
+              )
+            )
+                  )
+)
+)
+                  )
+)
